bfa4fa5046bd508338e32dce9d9b12f8386e4dde
Derbes Utebaliyev <uderbes@gmail.com>
2018-06-13 00:43:07 +0600

    first lab student

---
+++ student/student/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace student
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string last;
+            double gpa;
+            name = Console.ReadLine();
+            last = Console.ReadLine();
+            gpa = double.Parse(Console.ReadLine());
+            Student1 studentt = new Student1(name, last, gpa);
+            Console.WriteLine(studentt);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ student/student/Student1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace student
+{
+    class Student1
+    {
+        string name;
+        string last;
+        double gpa;
+
+        public Student1()
+        {
+            name = "Name";
+            last = "Last";
+            gpa = 0;
+        }
+
+        public Student1(string first,string second,double ball)
+        {
+            name = first;
+            last = second;
+            gpa = ball;
+        }
+        public override string ToString()
+        {
+            return name + " " + last + " " + gpa;
+        }
+    }
+}
